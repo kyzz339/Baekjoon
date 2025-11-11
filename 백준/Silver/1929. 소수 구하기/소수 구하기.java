999ce8ab22d810cb30e9a9c22885960540323779
JAEHYUN KWON <kyzz339@naver.com>
2025-11-11 22:20:54 +0900

    [Silver III] Title: 소수 구하기, Time: 952 ms, Memory: 37696 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int start_number = sc.nextInt();
+        int end_number = sc.nextInt();
+        boolean[] primes = new boolean[end_number + 1]; // true이면 소수 x
+        primes[0] = true;
+        primes[1] = true;
+        for(int i=2; i<=end_number; i++){
+            if(!primes[i]) {
+                for (int j = 2; j <= end_number; j++) {
+                    int prime_number = i * j;
+                    if (prime_number > end_number) break;
+                    primes[prime_number] = true;
+                }
+            }
+        }
+
+        for(int i=start_number; i<=end_number; i++){
+            if(!primes[i]){
+                System.out.println(i);
+            }
+        }
+    }
+}
