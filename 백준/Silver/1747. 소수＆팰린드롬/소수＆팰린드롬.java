817b8ff803826f2ce0c0234b15986da91cbfe269
JAEHYUN KWON <kyzz339@naver.com>
2025-11-17 21:59:41 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 176 ms, Memory: 23312 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int start_no = sc.nextInt();
+        int max_value = 2000000;
+        boolean[] primes = new boolean[max_value + 1];
+        int result = 0;
+        primes[0] = true;
+        primes[1] = true;
+
+        for(int i=2; i<=max_value; i++){
+
+            if(!primes[i]) {
+                for (int j = i + i; j <= max_value; j = j + i) {
+                    primes[j] = true;
+                }
+            }
+        }
+        for(int i=start_no ; i<= max_value; i++){
+            if(!primes[i]){
+                char[] arr = Integer.toString(i).toCharArray();
+                boolean flag = true;
+                for(int j=0; j<arr.length; j++){
+                    if(arr[j] != arr[arr.length - j - 1]){
+                        flag = false;
+                    }
+                }
+                if(flag) {
+                    result = i;
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
